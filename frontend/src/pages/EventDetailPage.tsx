b22ff42bf534078289b597acc852e0fa381a68af
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-05-10 07:25:01 -0500

    Output a list of dummy events to the EventsPage and when clicked output the  ID of the selected event on the EventDetailPage

--- frontend/src/pages/EventDetailPage.tsx
+++ frontend/src/pages/EventDetailPage.tsx
@@ -1,5 +1,13 @@
+import { Fragment } from "react";
+import { useParams } from "react-router-dom";
 function EventDetailPage() {
-  return <h1>EventDetailPage</h1>;
+  const params = useParams();
+
+  return (
+    <Fragment>
+      <h1>EventDetailPage</h1> <p>{params.id}</p>
+    </Fragment>
+  );
 }
 
 export default EventDetailPage;
--- frontend/src/pages/EventsPage.tsx
+++ frontend/src/pages/EventsPage.tsx
@@ -1,5 +1,32 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Some event",
+  },
+  {
+    id: "e2",
+    title: "Another event",
+  },
+];
+
 function EventsPage() {
-  return <h1>EventsPage</h1>;
+  return (
+    <Fragment>
+      <h1>EventsPage</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => {
+          return (
+            <li key={event.id}>
+              <Link to={event.id}>{event.id}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </Fragment>
+  );
 }
 
 export default EventsPage;
